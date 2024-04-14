7c3df23bac306ffb44d5657bbc1bb4564c8139ed
demondev0666 <demondev0666@gmail.com>
2024-04-14 00:49:47 -0400

    Changed Card Animation.

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Card(props) {
   return (
-    <div className="bg-Gray1 shadow-md p-6  mb-4 text-Cyan2 h-52 w-52 rounded-lg animate-heartbeat">
+    <div className="bg-Gray1 shadow-md p-6 mb-4 text-Cyan2 h-52 w-52 rounded-lg transition duration-300 ease-in-out hover:scale-105 hover:shadow-lg">
       {props.children}
     </div>
   );
